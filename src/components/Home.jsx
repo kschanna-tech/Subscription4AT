6804eb4b5d1f245b6c36618f6af5c7854569e0e2
kschanna-tech <channa.kanthisree@gmail.com>
2025-07-15 21:35:20 -0400

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Welcome to Subscription4AT!</h2>
+      <p>This app helps users manage their subscription plans effectively.</p>
+    </div>
+  );
+}
+
+export default Home;

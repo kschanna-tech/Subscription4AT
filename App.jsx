44fb3a0cd0e75e615f7306d8b85ed109958136df
kschanna-tech <channa.kanthisree@gmail.com>
2025-07-15 21:08:27 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-900 to-indigo-800 text-white">
+      <h1 className="text-4xl font-bold">Welcome to Subscription4AT</h1>
+    </div>
+  );
+}
+export default App;
